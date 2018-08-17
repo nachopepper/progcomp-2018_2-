76306beaf4ed4d8b69587c4ab62651ded7a50e84
nacho <nacho-udechile@live.cl>
2018-08-17 19:15:27 -0300

    Suma

---
+++ Resueltos/suma.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void suma(int a,int b){
+	
+	int sum=0;
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	printf("%d",sum);
+	
+	
+}
+
+
+
+int main(){
+	
+	int x,y;
+	
+	
+	suma(x,y);
+	
+	
+	
+	return 0;
+}
